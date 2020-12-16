567863f240cca8093b7f6ffea2d49a22826767f2
Sami <sami.el-harbil@uha.fr>
2020-12-16 19:42:25 +0100

    testing

--- Articles/controllers/Index.java
+++ Articles/controllers/Index.java
@@ -14,36 +14,41 @@ import javax.servlet.http.HttpServletResponse;
 public class Index extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-    /**
-     * Default constructor. 
-     */
-    public Index() {
-        // TODO Auto-generated constructor stub
-    }
+	/**
+	 * Default constructor.
+	 */
+	public Index() {
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
 		String user = request.getParameter("user");
 		String pass = request.getParameter("pass");
-		
-		if (user.equals("test") && pass.equals("test")) { 
+
+		if (user.equals("test") && pass.equals("test")) {
+
 			response.getWriter().append("Bonjour " + user);
+
+		} else {
+			response.getWriter().append("Mauvais identifiants (user : " + user + ", pass : " + pass + ")");
 		}
-		else {
-			response.getWriter().append("Mauvais identifiants (user : "+ user +", pass : "+pass+")");
-		}
-		
+
 	}
 
 }
